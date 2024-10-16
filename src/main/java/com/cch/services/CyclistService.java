a55b4824f0527a0e4b31f90d84a41a00947086be
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-10-16 15:11:55 +0100

    ajout de cyclist

---
+++ src/main/java/com/cch/services/CyclistService.java
@@ -0,0 +1,7 @@
+package com.cch.services;
+
+import com.cch.entities.Cyclist;
+
+public interface CyclistService {
+    Cyclist save(Cyclist cyclist);
+}
---
+++ src/main/java/com/cch/services/Impl/CyclistServiceImpl.java
@@ -0,0 +1,12 @@
+package com.cch.services.Impl;
+
+import com.cch.entities.Cyclist;
+import com.cch.services.CyclistService;
+
+public class CyclistServiceImpl implements CyclistService {
+
+    @Override
+    public Cyclist save(Cyclist cyclist) {
+        return null;
+    }
+}
