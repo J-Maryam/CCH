b9990ee77801f85d0115e1cb2deaf95441a9ee9b
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-10-16 14:38:46 +0100

    update configuration

--- src/main/java/com/cch/Main.java
+++ src/main/java/com/cch/Main.java
@@ -1,19 +1,8 @@
 package com.cch;
 
-import org.hibernate.SessionFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(com.cch.config.AppConfig.class);
-        SessionFactory sessionFactory = (SessionFactory) context.getBean("SessionFactory.class");
         System.out.print("Hello and welcome!");
 
     }
--- src/main/java/com/cch/config/AppConfig.java
+++ src/main/java/com/cch/config/AppConfig.java
@@ -1,20 +1,24 @@
 package com.cch.config;
 
-import org.apache.commons.dbcp2.BasicDataSource;
-import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
+@EnableTransactionManagement
 public class AppConfig {
 
     @Bean
     public DataSource dataSource() {
-        BasicDataSource dataSource = new BasicDataSource();
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
         dataSource.setUrl("jdbc:postgresql://localhost:5432/cch");
         dataSource.setUsername("postgres");
@@ -23,13 +27,33 @@ public class AppConfig {
     }
 
     @Bean
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan("com.cch.model");
-        sessionFactory.getHibernateProperties().setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        sessionFactory.getHibernateProperties().setProperty("hibernate.show_sql", "true");
-        sessionFactory.getHibernateProperties().setProperty("hibernate.hbm2ddl.auto", "update");
-        return sessionFactory;
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean emFactory = new LocalContainerEntityManagerFactoryBean();
+        emFactory.setDataSource(dataSource);
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        emFactory.setJpaVendorAdapter(vendorAdapter);
+
+        emFactory.setPackagesToScan("com.cch.entities");
+
+        emFactory.setJpaProperties(hibernateProperties());
+
+        return emFactory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory.getObject()); // Utilisation de l'EntityManagerFactory
+        return transactionManager;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("hibernate.format_sql", "true");
+        return properties;
     }
 }
